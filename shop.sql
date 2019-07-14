381ca3878911d2f87d5b8f7bf24213c501eb4c62
Ardy Junata <ardyjunata68@gmail.com>
2019-07-15 00:17:24 +0700

    tambah sql

---
+++ shop.sql
@@ -0,0 +1,395 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 14 Jul 2019 pada 19.16
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `optiongroups`
+--
+
+CREATE TABLE `optiongroups` (
+  `OptionGroupID` int(11) NOT NULL,
+  `OptionGroupName` varchar(50) COLLATE latin1_german2_ci DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+
+--
+-- Dumping data untuk tabel `optiongroups`
+--
+
+INSERT INTO `optiongroups` (`OptionGroupID`, `OptionGroupName`) VALUES
+(1, 'color'),
+(2, 'size');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `options`
+--
+
+CREATE TABLE `options` (
+  `OptionID` int(11) NOT NULL,
+  `OptionGroupID` int(11) DEFAULT NULL,
+  `OptionName` varchar(50) COLLATE latin1_german2_ci DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+
+--
+-- Dumping data untuk tabel `options`
+--
+
+INSERT INTO `options` (`OptionID`, `OptionGroupID`, `OptionName`) VALUES
+(1, 1, 'red'),
+(2, 1, 'blue'),
+(3, 1, 'green'),
+(4, 2, 'S'),
+(5, 2, 'M'),
+(6, 2, 'L'),
+(7, 2, 'XL'),
+(8, 2, 'XXL');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `orderdetails`
+--
+
+CREATE TABLE `orderdetails` (
+  `DetailID` int(11) NOT NULL,
+  `DetailOrderID` int(11) NOT NULL,
+  `DetailProductID` int(11) NOT NULL,
+  `DetailName` varchar(250) COLLATE latin1_german2_ci NOT NULL,
+  `DetailPrice` float NOT NULL,
+  `DetailSKU` varchar(50) COLLATE latin1_german2_ci NOT NULL,
+  `DetailQuantity` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderID` int(11) NOT NULL,
+  `OrderUserID` int(11) NOT NULL,
+  `OrderAmount` float NOT NULL,
+  `OrderShipName` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `OrderShipAddress` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `OrderShipAddress2` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `OrderCity` varchar(50) COLLATE latin1_german2_ci NOT NULL,
+  `OrderState` varchar(50) COLLATE latin1_german2_ci NOT NULL,
+  `OrderZip` varchar(20) COLLATE latin1_german2_ci NOT NULL,
+  `OrderCountry` varchar(50) COLLATE latin1_german2_ci NOT NULL,
+  `OrderPhone` varchar(20) COLLATE latin1_german2_ci NOT NULL,
+  `OrderFax` varchar(20) COLLATE latin1_german2_ci NOT NULL,
+  `OrderShipping` float NOT NULL,
+  `OrderTax` float NOT NULL,
+  `OrderEmail` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `OrderDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `OrderShipped` tinyint(1) NOT NULL DEFAULT '0',
+  `OrderTrackingNumber` varchar(80) COLLATE latin1_german2_ci DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `productcategories`
+--
+
+CREATE TABLE `productcategories` (
+  `CategoryID` int(11) NOT NULL,
+  `CategoryName` varchar(50) COLLATE latin1_german2_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+
+--
+-- Dumping data untuk tabel `productcategories`
+--
+
+INSERT INTO `productcategories` (`CategoryID`, `CategoryName`) VALUES
+(1, 'Model Kit'),
+(2, 'Gundam'),
+(3, 'Action Figure Lain');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `productoptions`
+--
+
+CREATE TABLE `productoptions` (
+  `ProductOptionID` int(10) UNSIGNED NOT NULL,
+  `ProductID` int(10) UNSIGNED NOT NULL,
+  `OptionID` int(10) UNSIGNED NOT NULL,
+  `OptionPriceIncrement` double DEFAULT NULL,
+  `OptionGroupID` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+
+--
+-- Dumping data untuk tabel `productoptions`
+--
+
+INSERT INTO `productoptions` (`ProductOptionID`, `ProductID`, `OptionID`, `OptionPriceIncrement`, `OptionGroupID`) VALUES
+(1, 1, 1, 0, 1),
+(2, 1, 2, 0, 1),
+(3, 1, 3, 0, 1),
+(4, 1, 4, 0, 2),
+(5, 1, 5, 0, 2),
+(6, 1, 6, 0, 2),
+(7, 1, 7, 2, 2),
+(8, 1, 8, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `products`
+--
+
+CREATE TABLE `products` (
+  `ProductID` int(12) NOT NULL,
+  `ProductSKU` varchar(50) COLLATE latin1_german2_ci NOT NULL,
+  `ProductName` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `ProductPrice` float NOT NULL,
+  `ProductWeight` float NOT NULL,
+  `ProductCartDesc` varchar(250) COLLATE latin1_german2_ci NOT NULL,
+  `ProductShortDesc` varchar(1000) COLLATE latin1_german2_ci NOT NULL,
+  `ProductLongDesc` text COLLATE latin1_german2_ci NOT NULL,
+  `ProductThumb` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `ProductImage` varchar(100) COLLATE latin1_german2_ci NOT NULL,
+  `ProductCategoryID` int(11) DEFAULT NULL,
+  `ProductUpdateDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ProductStock` float DEFAULT NULL,
+  `ProductLive` tinyint(1) DEFAULT '0',
+  `ProductUnlimited` tinyint(1) DEFAULT '1',
+  `ProductLocation` varchar(250) COLLATE latin1_german2_ci DEFAULT NULL,
+  `CategoryID` int(11) DEFAULT NULL,
+  `ProductImg` varchar(100) COLLATE latin1_german2_ci DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+
+--
+-- Dumping data untuk tabel `products`
+--
+
+INSERT INTO `products` (`ProductID`, `ProductSKU`, `ProductName`, `ProductPrice`, `ProductWeight`, `ProductCartDesc`, `ProductShortDesc`, `ProductLongDesc`, `ProductThumb`, `ProductImage`, `ProductCategoryID`, `ProductUpdateDate`, `ProductStock`, `ProductLive`, `ProductUnlimited`, `ProductLocation`, `CategoryID`, `ProductImg`) VALUES
+(1, '', 'PG ASTRAY BLUE FRAME', 2000000, 0, '', '', '', '', '', NULL, '2019-07-13 15:27:29', NULL, 0, 1, NULL, 1, 'PG ASTRAY BLUE FRAME.jpg'),
+(2, '000-0004', 'PG EXIA REPAIR', 1000000, 8, 'Track and Trail', 'A rugged track and trail athletic shoe', 'A rugged track and trail athletic shoe', '', '', 4, '2013-07-25 19:04:36', NULL, 0, 1, NULL, 3, 'PG EXIA REPAIR.jpg'),
+(3, '', 'PG UNICORN PHENEX', 3000000, 0, '', '', '', '', '', NULL, '2019-07-13 15:36:07', NULL, 0, 1, NULL, 2, 'PG UNICORN PHENEX.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `shopingcart`
+--
+
+CREATE TABLE `shopingcart` (
+  `CartID` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `TotalPice` int(11) NOT NULL,
+  `ProductID` int(12) DEFAULT NULL,
+  `userID` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `shopingcart`
+--
+
+INSERT INTO `shopingcart` (`CartID`, `Quantity`, `TotalPice`, `ProductID`, `userID`) VALUES
+(1, 7, 14000000, 1, 1),
+(5, 7, 14000000, 1, 2),
+(7, 1, 2000000, 3, 2),
+(8, 7, 14000000, 1, 2),
+(9, 7, 14000000, 1, 2),
+(10, 1, 1000000, 2, 2),
+(11, 7, 14000000, 1, 2),
+(12, 7, 14000000, 1, 2),
+(13, 7, 14000000, 1, 2),
+(14, 7, 14000000, 1, 2),
+(15, 7, 14000000, 1, 2),
+(16, 1, 2000000, 1, 2),
+(17, 1, 1000000, 2, 2),
+(18, 1, 2000000, 1, 2),
+(19, 1, 2000000, 1, 2),
+(20, 1, 1000000, 2, 2),
+(21, 1, 1000000, 2, 2),
+(22, 1, 2000000, 1, 2),
+(23, 1, 2000000, 1, 1),
+(24, 1, 1000000, 2, 1),
+(25, 1, 3000000, 3, 1),
+(26, 1, 1000000, 2, 2),
+(27, 1, 3000000, 3, 1),
+(28, 1, 1000000, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(11) NOT NULL,
+  `UserEmail` varchar(500) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserPassword` varchar(500) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserFirstName` varchar(50) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserLastName` varchar(50) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserCity` varchar(90) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserState` varchar(20) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserZip` varchar(12) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserEmailVerified` tinyint(1) DEFAULT '0',
+  `UserRegistrationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `UserVerificationCode` varchar(20) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserIP` varchar(50) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserPhone` varchar(20) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserFax` varchar(20) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserCountry` varchar(20) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserAddress` varchar(100) COLLATE latin1_german2_ci DEFAULT NULL,
+  `UserAddress2` varchar(50) COLLATE latin1_german2_ci DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`UserID`, `UserEmail`, `UserPassword`, `UserFirstName`, `UserLastName`, `UserCity`, `UserState`, `UserZip`, `UserEmailVerified`, `UserRegistrationDate`, `UserVerificationCode`, `UserIP`, `UserPhone`, `UserFax`, `UserCountry`, `UserAddress`, `UserAddress2`) VALUES
+(1, 'ardy', '1', NULL, NULL, NULL, NULL, NULL, 0, '2019-07-14 06:31:53', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 'tyo', '1', NULL, NULL, NULL, NULL, NULL, 0, '2019-07-14 11:51:21', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `optiongroups`
+--
+ALTER TABLE `optiongroups`
+  ADD PRIMARY KEY (`OptionGroupID`);
+
+--
+-- Indeks untuk tabel `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`OptionID`);
+
+--
+-- Indeks untuk tabel `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  ADD PRIMARY KEY (`DetailID`);
+
+--
+-- Indeks untuk tabel `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderID`);
+
+--
+-- Indeks untuk tabel `productcategories`
+--
+ALTER TABLE `productcategories`
+  ADD PRIMARY KEY (`CategoryID`);
+
+--
+-- Indeks untuk tabel `productoptions`
+--
+ALTER TABLE `productoptions`
+  ADD PRIMARY KEY (`ProductOptionID`);
+
+--
+-- Indeks untuk tabel `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ProductID`),
+  ADD KEY `fk_category` (`CategoryID`);
+
+--
+-- Indeks untuk tabel `shopingcart`
+--
+ALTER TABLE `shopingcart`
+  ADD PRIMARY KEY (`CartID`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `optiongroups`
+--
+ALTER TABLE `optiongroups`
+  MODIFY `OptionGroupID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `options`
+--
+ALTER TABLE `options`
+  MODIFY `OptionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  MODIFY `DetailID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `productcategories`
+--
+ALTER TABLE `productcategories`
+  MODIFY `CategoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `productoptions`
+--
+ALTER TABLE `productoptions`
+  MODIFY `ProductOptionID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `products`
+--
+ALTER TABLE `products`
+  MODIFY `ProductID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=994;
+
+--
+-- AUTO_INCREMENT untuk tabel `shopingcart`
+--
+ALTER TABLE `shopingcart`
+  MODIFY `CartID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
