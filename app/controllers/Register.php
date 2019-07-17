91033f18a43ce4d657296cbb160bbc5efd4f9066
Ardy Junata <ardyjunata68@gmail.com>
2019-07-17 22:51:54 +0700

    regis selesai

--- app/controllers/Register.php
+++ app/controllers/Register.php
@@ -3,8 +3,19 @@
 class Register extends Controller {
     public function index()
     {
+        $data['judul'] = 'Register';
         $this->view('templates/css', $data);
         $this->view('register/index');
         $this->view('templates/script');
+    }
+
+    public function userAdd()
+    {
+        if( $this->model('Register_model')->insertUser($_POST) > 0)
+        {
+            header('Location:' . BASEURL . '/login');
+        } else {
+            echo "Error";
+        }
     }
 }
--- app/views/register/index.php
+++ app/views/register/index.php
@@ -7,31 +7,32 @@
 					<div class="login_box_img">
 						<img class="img-fluid" src="img/login.jpg" alt="">
 						<div class="hover">
-							<h4>Dont have an account ?</h4>
-							<p>You can register your account by click button below</p>
-							<a class="primary-btn" href="registration.html">Create an Account</a>
+							<h4>Have an account ?</h4>
+							<p>You can login your account by clik button bellow</p>
+							<a class="primary-btn" href="<?= BASEURL; ?>/login">Login</a>
 						</div>
 					</div>
 				</div>
 				<div class="col-lg-6">
 					<div class="login_form_inner">
-						<h3>Log in to enter</h3>
-						<form class="row login_form" action="<?= BASEURL ?>/login/cek" method="post" id="contactForm" novalidate="novalidate">
+						<h3>Register Form</h3>
+						<form class="row login_form" action="<?= BASEURL ?>/register/userAdd" method="post" id="contactForm" novalidate="novalidate">
 							<div class="col-md-12 form-group">
 								<input type="text" class="form-control" id="name" name="name" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'">
 							</div>
 							<div class="col-md-12 form-group">
 								<input type="password" class="form-control" id="password" name="password" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
+                            </div>
+                            <div class="col-md-12 form-group">
+								<input type="text" class="form-control" id="address" name="address" placeholder="Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'">
+                            </div>
+                            <div class="col-md-12 form-group">
+								<input type="text" class="form-control" id="number" name="number" placeholder="Phone Number" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'">
 							</div>
 							<div class="col-md-12 form-group">
-								<div class="creat_account">
-									<input type="checkbox" id="f-option2" name="selector">
-									<label for="f-option2">Keep me logged in</label>
-								</div>
 							</div>
 							<div class="col-md-12 form-group">
-								<button type="submit" name="login" value="submit" class="primary-btn">Log In</button>
-								<a href="#">Forgot Password?</a>
+								<button type="submit" name="regis" value="submit" class="primary-btn">Register</button>
 								<a href="<?= BASEURL?>/login/api" class="text-primary">Login With Google</a>
 							</div>
 						</form>
